1c6037f991f2169e948ddc7f6edf6154655d180e
mattmorgandev <mattmorganofficial@gmail.com>
2024-04-11 00:44:25 -0400

    linting 12

--- server/database/app.js
+++ server/database/app.js
@@ -8,32 +8,28 @@ var port = 3030;
 app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-var reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
-var dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
+var reviewsData = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
+var dealershipsData = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/", {'dbName': 'dealershipsDB'});
-
+mongoose.connect("mongodb://mongo_db:27017/", { dbName: 'dealershipsDB' });
 
 var Reviews = require('./review');
-
 var Dealerships = require('./dealership');
 
 try {
-  Reviews.deleteMany({}).then(function(){
-    Reviews.insertMany(reviews_data['reviews']);
+  Reviews.deleteMany({}).then(function() {
+    Reviews.insertMany(reviewsData.reviews);
   });
-  Dealerships.deleteMany({}).then(function(){
-    Dealerships.insertMany(dealerships_data['dealerships']);
+  Dealerships.deleteMany({}).then(function() {
+    Dealerships.insertMany(dealershipsData.dealerships);
   });
-  
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
 }
 
-
 // Express route to home
 app.get('/', async function(req, res) {
-    res.send("Welcome to the Mongoose API")
+  res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -49,7 +45,7 @@ app.get('/fetchReviews', async function(req, res) {
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async function(req, res) {
   try {
-    var documents = await Reviews.find({dealership: req.params.id});
+    var documents = await Reviews.find({ dealership: req.params.id });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -58,81 +54,80 @@ app.get('/fetchReviews/dealer/:id', async function(req, res) {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async function(req, res) {
-    try{
-        var dealers = await Dealerships.find();
-        res.json(dealers);
-    } catch(error){
-        res.status(500).json({ error: 'Error fetching documents'});
-    }
+  try {
+    var dealers = await Dealerships.find();
+    res.json(dealers);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async function(req, res) {
-    try{
-        var dealers = await Dealerships.find({state: req.params.state});
-        res.json(dealers);
-    } catch (err){
-        res.status(500).json({ error: 'Error fetching documents'});
-    }
+  try {
+    var dealers = await Dealerships.find({ state: req.params.state });
+    res.json(dealers);
+  } catch (err) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async function(req, res) {
-    try{
-        var documents = await Dealerships.find({id: req.params.id});
-        res.json(documents);
-    } catch (err){
-        res.status(500).json({ error: 'Error fetching documents'});
-    }
+  try {
+    var documents = await Dealerships.find({ id: req.params.id });
+    res.json(documents);
+  } catch (err) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
-//Express route to insert review
+// Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async function(req, res) {
   var data = JSON.parse(req.body);
-  var documents = await Reviews.find().sort( { id: -1 } )
-  var new_id = documents[0]['id']+1
+  var documents = await Reviews.find().sort({ id: -1 });
+  var newId = documents[0].id + 1;
 
   var review = new Reviews({
-		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
-	});
+    id: newId,
+    name: data.name,
+    dealership: data.dealership,
+    review: data.review,
+    purchase: data.purchase,
+    purchase_date: data.purchase_date,
+    car_make: data.car_make,
+    car_model: data.car_model,
+    car_year: data.car_year,
+  });
 
   try {
     var savedReview = await review.save();
     res.json(savedReview);
   } catch (error) {
-		console.log(error);
+    console.log(error);
     res.status(500).json({ error: 'Error inserting review' });
   }
 });
 
 // Express route to fetch reviews by car make
 app.get('/fetchReviews/car/:carmake', async function(req, res) {
-    try {
-      var documents = await Reviews.find({ car_make: req.params.carmake });
-      res.json(documents);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching documents' });
-    }
-  });
-  
-  // Express route to fetch reviews by car make and model
-  app.get('/fetchReviews/car/:carmake/:model', async function(req, res) {
-    try {
-      var documents = await Reviews.find({ car_make: req.params.carmake, car_model: req.params.model });
-      res.json(documents);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching documents' });
-    }
-  });
-  
+  try {
+    var documents = await Reviews.find({ car_make: req.params.carmake });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
+
+// Express route to fetch reviews by car make and model
+app.get('/fetchReviews/car/:carmake/:model', async function(req, res) {
+  try {
+    var documents = await Reviews.find({ car_make: req.params.carmake, car_model: req.params.model });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
 
 // Start the Express server
 app.listen(port, function() {
